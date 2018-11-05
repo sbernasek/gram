335cd5895f37de5020f5735d544a38c7e9257377
Sebastian Bernasek <sebastian@u.northwestern.edu>
2018-11-05 15:46:25 -0600

    fixed args

--- gram/simulation/pulse.py
+++ gram/simulation/pulse.py
@@ -139,8 +139,9 @@ class PulseSimulation:
         # run ODE solver (ignoring divide by zero warning)
         with warnings.catch_warnings():
             warnings.filterwarnings("ignore", category=RuntimeWarning)
-            results = simulation.solve_ivp(signal=signal,
-                                           duration=2*self.simulation_duration)
+            results = simulation.solve_ivp(
+                signal=signal,
+                duration=2*self.simulation_duration)
 
         # take last timepoint as steady state
         ss = results.mean[:, -1]
